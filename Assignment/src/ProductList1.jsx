59443a1713f30f00eac2e4055b70571be16fbd94
slyvia <slyviafams@gmail.com>
2024-11-13 17:59:51 +0700

    Question 5

--- Assignment/src/ProductList1.jsx
+++ Assignment/src/ProductList1.jsx
@@ -4,11 +4,10 @@ function ProductList(props) {
   const products = props.products;
   const productListItems = [];
 
-
   products.forEach((product, index) => {
     productListItems.push(
       <li key={index}>
-        {product.name}: ${product.price}
+        {product.name}: {product.inStock ? 'In Stock' : 'Out of Stock'}
       </li>
     );
   });
